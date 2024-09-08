d222bd1166484a5c4601c0b64420db07fbd483bf
ZhiQing-R <119010253@link.cuhk.edu.cn>
2024-09-07 22:14:06 -0400

    finish base part

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ const Stats = require('stats-js');
 import * as DAT from 'dat.gui';
 import Icosphere from './geometry/Icosphere';
 import Square from './geometry/Square';
+import Cube from './geometry/Cube';
 import OpenGLRenderer from './rendering/gl/OpenGLRenderer';
 import Camera from './Camera';
 import {setGL} from './globals';
@@ -12,18 +13,23 @@ import ShaderProgram, {Shader} from './rendering/gl/ShaderProgram';
 // This will be referred to by dat.GUI's functions that add GUI elements.
 const controls = {
   tesselations: 5,
+  color: [255, 0, 0],
   'Load Scene': loadScene, // A function pointer, essentially
 };
 
 let icosphere: Icosphere;
 let square: Square;
+let cube: Cube;
 let prevTesselations: number = 5;
+let time : number = 0;
 
 function loadScene() {
   icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, controls.tesselations);
   icosphere.create();
   square = new Square(vec3.fromValues(0, 0, 0));
   square.create();
+  cube = new Cube(vec3.fromValues(0, 0, 0));
+  cube.create();
 }
 
 function main() {
@@ -39,6 +45,7 @@ function main() {
   const gui = new DAT.GUI();
   gui.add(controls, 'tesselations', 0, 8).step(1);
   gui.add(controls, 'Load Scene');
+  gui.addColor(controls, 'color');
 
   // get canvas and webgl context
   const canvas = <HTMLCanvasElement> document.getElementById('canvas');
@@ -66,6 +73,7 @@ function main() {
 
   // This function will be called every frame
   function tick() {
+    ++time;
     camera.update();
     stats.begin();
     gl.viewport(0, 0, window.innerWidth, window.innerHeight);
@@ -76,10 +84,21 @@ function main() {
       icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, prevTesselations);
       icosphere.create();
     }
+
+    const colorCtl = new Float32Array([
+      controls.color[0] / 255.0,
+      controls.color[1] / 255.0,
+      controls.color[2] / 255.0,
+      1.0
+  ]);
+
+    lambert.setTime(time);
+
     renderer.render(camera, lambert, [
-      icosphere,
+      // icosphere,
+      cube,
       // square,
-    ]);
+    ], colorCtl);
     stats.end();
 
     // Tell the browser to call `tick` again whenever it renders a new frame
--- src/shaders/lambert-frag.glsl
+++ src/shaders/lambert-frag.glsl
@@ -13,14 +13,45 @@ precision highp float;
 
 uniform vec4 u_Color; // The color with which to render this instance of geometry.
 
+uniform float u_Time;
+
 // These are the interpolated values out of the rasterizer, so you can't know
 // their specific values without knowing the vertices that contributed to them
 in vec4 fs_Nor;
 in vec4 fs_LightVec;
 in vec4 fs_Col;
+in vec4 fs_Pos;
 
 out vec4 out_Col; // This is the final output color that you will see on your
                   // screen for the pixel that is currently being processed.
+
+vec3 hash( vec3 x )
+{
+	x = vec3( dot(x,vec3(127.1,311.7, 74.7)),
+			  dot(x,vec3(269.5,183.3,246.1)),
+			  dot(x,vec3(113.5,271.9,124.6)));
+
+	return fract(sin(x)*43758.5453123);
+}
+
+float voronoi( in vec3 x )
+{
+    vec3 p = floor( x );
+    vec3 f = fract( x );
+
+    float res = 100.f;
+    for( int k=-1; k<=1; k++ )
+    for( int j=-1; j<=1; j++ )
+    for( int i=-1; i<=1; i++ )
+    {
+        vec3 b = vec3( float(i), float(j), float(k) );
+        vec3 r = vec3( b ) - f + hash( p + b );
+        float d = dot( r, r );
+        res = min(d, res);
+    }
+
+    return res;
+}
 
 void main()
 {
@@ -30,7 +61,7 @@ void main()
         // Calculate the diffuse term for Lambert shading
         float diffuseTerm = dot(normalize(fs_Nor), normalize(fs_LightVec));
         // Avoid negative lighting values
-        // diffuseTerm = clamp(diffuseTerm, 0, 1);
+        diffuseTerm = max(diffuseTerm, 0.f);
 
         float ambientTerm = 0.2;
 
@@ -38,6 +69,9 @@ void main()
                                                             //to simulate ambient lighting. This ensures that faces that are not
                                                             //lit by our point light are not completely black.
 
+        float noise = voronoi(2.f * fs_Pos.xyz + u_Time * 0.01f);
+        noise = 0.4f + 0.8f * noise;
+
         // Compute final shaded color
-        out_Col = vec4(diffuseColor.rgb * lightIntensity, diffuseColor.a);
+        out_Col = vec4(diffuseColor.rgb * lightIntensity * noise, diffuseColor.a);
 }
